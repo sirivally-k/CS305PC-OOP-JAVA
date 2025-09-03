be6ff53b7e2241cbdce1524edbadfeee5f70fc59
sirivally-k <sirivally733060@gmail.com>
2025-09-03 11:30:04 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,88 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b = 11;
+		Byte bb = b;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);	
+		System.out.println("---------------integer---");
+		
+		int i = 11;
+		Integer ii = i;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+		System.out.println(ii.BYTES);	
+		System.out.println("---------------short---");
+		
+		short s = 11;
+		Short ss = s;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.BYTES);	
+		System.out.println("---------------long---");
+		
+		long l = 11;
+		Long ll = l;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+		System.out.println(ll.BYTES);	
+		System.out.println("---------------float---");
+		
+		float f = 11;
+		Float ff = f;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(ff.SIZE);
+		System.out.println(ff.TYPE);
+		System.out.println(ff.BYTES);	
+		System.out.println("---------------double---");
+		
+		double d = 11;
+		Double dd = d;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.BYTES);	
+		System.out.println("------------------boolean--");
+		
+		boolean bl = 11;
+		Boolean blbl = bl;//Autoboxing
+		
+		//int x =200;
+		//byte b = x;
+		System.out.println(Boolean.MAX_VALUE);
+		System.out.println(blbl.MIN_VALUE);
+		System.out.println(blbl.SIZE);
+		System.out.println(blbl.TYPE);
+		System.out.println(blbl.BYTES);
+		
+		
+	}
+}
