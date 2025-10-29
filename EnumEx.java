104029f8d9c533961d595f722c10a45f405df196
sirivally-k <sirivally733060@gmail.com>
2025-10-29 12:04:30 +0530

    Add files via upload

---
+++ EnumEx.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+enum Fruits{	Apple('a'),Banana('b'),Citrus('c'),DragonFruit('d'),Fig('f'),Grapes('g');
+	char a;
+ 	Fruits(char a){
+ 		this.a = a;
+ 	}
+ 	public char get(){
+ 		return a;
+ 	}
+ }
+public class EnumEx{
+	public static void main(String...args){
+		Scanner s=new Scanner(System.in);
+		String st=s.next();
+		char ch = st.charAt(0);
+		ch = Character.toLowerCase(ch);
+		for(Fruits f:Fruits.values()){
+			if(f.get() == ch)
+			System.out.println(f);
+		}
+		
+
+	}
+}
